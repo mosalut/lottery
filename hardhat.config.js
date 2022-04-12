a98743012f1acb50c98b647101b25a0add33d1ac
mosalut <ludaoyuan@mosalut.com>
2022-04-13 01:33:07 +0800

    Squashed commit of the following:
    commit 194de1a5d6621dce134da454f90e62a2caef8d37
    Author: mosalut <ludaoyuan@mosalut.com>
    Date:   Wed Apr 13 01:25:39 2022 +0800
    
        Turn the pool address to the contract lottery address
    
        Repair bugs:
        	chip in:
        		update the totalstake of order number.
        		update the pool of order number.
    
        	functions:
        		runingRound() view public returns(uint128)
    
        	test:
        		Test the win condition success
    
        	modified:   contracts/Lottery.sol
        	modified:   test/test.js
    
    commit e07e2b043727fdc84a3b34c7c1c03f1ca74ba2af
    Author: mosalut <ludaoyuan@mosalut.com>
    Date:   Tue Apr 12 22:53:35 2022 +0800
    
        Use contract Dai address as rewards pool.
        Finished chipIn function.
    
                functions:
                        history(uint128 _orderNumber) external view returns (uint16, address[] memory)
                        historyWinnerStakes(uint128 _orderNumber, address account) external view returns (uint)
                        newest() external view returns (uint128)
                        lastStake(address account) external view returns (uint128, uint, uint16, uint16, uint)
    
        Write deploy code.(have not been tested)
        Write test several code.
    
        Modify hardhat.config.js
        	set optimezer
        	set testnet
    
                modified:   contracts/Dai.sol
                modified:   contracts/Lottery.sol
                modified:   hardhat.config.js
                modified:   package-lock.json
                modified:   package.json
                new file:   scripts/deploy.js
                deleted:    scripts/sample-script.js
                deleted:    test/sample-test.js
                new file:   test/test.js
    
    commit ec2abfb7d933769515642aee55b0b48f9e07159d
    Author: mosalut <ludaoyuan@mosalut.com>
    Date:   Tue Apr 12 17:02:59 2022 +0800
    
        Build the arch of the DAPP
    
        Create 2 contract:
        	Dai.sol Dai
        	Lottery.sol Lottery
    
        	Dai is an token ERC20
        	Lottery is the main contract
    
        	functions in Lottery:
        		createRound(uint _timestamp) external onlyOwner
        		chipIn(uint stake, uint16 numbers) external
        		countWinStakes(uint128 _orderNumber) view internal returns (uint)
        		history(uint128 _orderNumber) external view returns (uint16, address[] memory)
        		historyWinnerStakes(uint128 _orderNumber, address account) external view returns (uint)
    
        	new file:   contracts/Dai.sol
        	deleted:    contracts/Greeter.sol
        	new file:   contracts/Lottery.sol
    
        	modified:   contracts/Dai.sol
        	modified:   contracts/Lottery.sol
        	modified:   hardhat.config.js
        	modified:   package-lock.json
        	modified:   package.json
    
    commit be7c57605f565c321523ca47ee2e80eae1dd954d
    Author: mosalut <ludaoyuan@mosalut.com>
    Date:   Tue Apr 12 11:08:14 2022 +0800
    
        Init:
        	Install hardhat sample project modules
    
        	new file:   .gitignore
        	new file:   contracts/Greeter.sol
        	new file:   hardhat.config.js
        	new file:   package-lock.json
        	new file:   package.json
        	new file:   scripts/sample-script.js
        	new file:   test/sample-test.js

---
+++ hardhat.config.js
@@ -0,0 +1,41 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+	const accounts = await hre.ethers.getSigners();
+
+	for (const account of accounts) {
+		console.log(account.address);
+	}
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+	solidity: {
+		version: "0.8.9",
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 1000
+			}
+		}
+	},
+	defaultNetwork: "localhost",
+	networks: {
+		localhost: {
+			url: "http://0.0.0.0:8545",
+		},
+		testnet: {
+			url: "https://eth-ropsten.alchemyapi.io/v2/q9dkePbPFo_irrYcnQdyYegtY5z8XcK-",
+			chainId: 97,
+			gasPrice: 20000000000,
+			accounts: ["0x6473d773ad0941f0b096083b9823435c5f955bef3f878578b3cf7856dec81779"]
+		}
+	}
+};
